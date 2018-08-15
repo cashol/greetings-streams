937a38b41c5bf279ea9f2443f1215cc46616b5be
cashol <bw1cash0@MD1KV7EC.ad001.siemens.net>
2018-08-15 20:00:41 +0200

    ExceptionHandler in GreetingsController introduced.

--- src/main/java/com/example/web/GreetingsController.java
+++ src/main/java/com/example/web/GreetingsController.java
@@ -1,10 +1,14 @@
 package com.example.web;
 
+import java.io.IOException;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletResponse;
 
 import com.example.model.Greetings;
 import com.example.service.GreetingsService;
@@ -17,18 +21,22 @@ public class GreetingsController {
     this.greetingsService = greetingsService;
   }
 
+  // http://localhost:8080/greetings?message=hello
   @GetMapping("/greetings")
   @ResponseStatus(HttpStatus.ACCEPTED)
   public void greetings(@RequestParam("message") String message) {
-//    Greetings greetings = new Greetings();
-//    greetings.setMessage(message);
-//    greetings.setTimestamp(System.currentTimeMillis());
-    
     Greetings greetings = Greetings.builder()
         .message(message)
         .timestamp(System.currentTimeMillis())
         .build();
     
     greetingsService.sendGreeting(greetings);
+  }
+  
+  @ExceptionHandler
+  void handleIllegalArgumentException(
+    IllegalArgumentException e, HttpServletResponse response) throws IOException {
+ 
+    response.sendError(HttpStatus.BAD_REQUEST.value());
   }
 }
